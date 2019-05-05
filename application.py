54c1af0da05ebc914403a6c0b478ec04ca54ceab
christopher.schock <christopher.schock@state.co.us>
2019-05-05 10:18:36 -0600

    added page refresh

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ def hello():
     ps = subprocess.Popen(('date', '+%a %H:%M:%S'), stdout=subprocess.PIPE)
     output = subprocess.check_output(('/usr/bin/figlet', '-f', '/tmp/figlet-2.2.5/fonts/standard.flf'), stdin=ps.stdout)
     ps.wait()
-    return '<p><pre>' + str(output, 'utf-8') + '</pre></p>'
+    return '<meta http-equiv="refresh" content="5" ><p><pre>' + str(output, 'utf-8') + '</pre></p>'
     #return '<p><pre>hello world</pre></p>'
 if __name__ == "__main__":
       application.run()
