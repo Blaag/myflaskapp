491e480ede10b2725cd5dd95fb88baa482d04f4e
christopher.schock <christopher.schock@state.co.us>
2019-05-04 22:39:45 -0600

    removed figlet for now

--- application.py
+++ application.py
@@ -8,9 +8,10 @@ application = Flask(__name__)
 application.debug = True
 @application.route('/', methods=['GET'])
 def hello():
-    ps = subprocess.Popen(('date', '+%a %H:%M:%S'), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('figlet'), stdin=ps.stdout)
-    ps.wait()
-    return '<p><pre>' + str(output, 'utf-8') + '</pre></p>'
+    #ps = subprocess.Popen(('date', '+%a %H:%M:%S'), stdout=subprocess.PIPE)
+    #output = subprocess.check_output(('figlet'), stdin=ps.stdout)
+    #ps.wait()
+    #return '<p><pre>' + str(output, 'utf-8') + '</pre></p>'
+    return '<p><pre>hello world</pre></p>'
 if __name__ == "__main__":
       application.run()
