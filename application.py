0874cc697694adc57f35547745a63cd55a7a1d2e
christopher.schock <christopher.schock@state.co.us>
2019-05-04 21:39:40 -0600

    initial commit

---
+++ application.py
@@ -0,0 +1,16 @@
+import subprocess
+
+from flask import Flask
+from subprocess import Popen, PIPE
+
+
+application = Flask(__name__)
+application.debug = True
+@application.route('/', methods=['GET'])
+def hello():
+    ps = subprocess.Popen(('date', '+%a %H:%M:%S'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('figlet'), stdin=ps.stdout)
+    ps.wait()
+    return '<p><pre>' + str(output, 'utf-8') + '</pre></p>'
+if __name__ == "__main__":
+      application.run()
