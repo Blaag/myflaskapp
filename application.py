188d76e65f2a3c8569cc7343f9a062d6e147c4ff
christopher.schock <christopher.schock@state.co.us>
2019-05-05 09:40:53 -0600

    added pathing and args to figlet

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ application.debug = True
 @application.route('/', methods=['GET'])
 def hello():
     ps = subprocess.Popen(('date', '+%a %H:%M:%S'), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('figlet'), stdin=ps.stdout)
+    output = subprocess.check_output(('figlet', '-f', '/usr/bin/standard.flf'), stdin=ps.stdout)
     ps.wait()
     return '<p><pre>' + str(output, 'utf-8') + '</pre></p>'
     #return '<p><pre>hello world</pre></p>'
